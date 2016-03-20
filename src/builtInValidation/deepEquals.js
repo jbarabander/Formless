ce350484084253b71aeee6b7e003089b398f742a
Justin Barabander <jbarabander@gmail.com>
2016-03-19 22:56:28 -0400

    Added in more validators

---
+++ src/builtInValidation/deepEquals.js
@@ -0,0 +1,22 @@
+module.exports = function(prop, param) {
+	if(typeof param !== 'object' || typeof prop !== 'object') {
+		return false;
+	}
+	var propKeys = 
+
+}
+
+function jsonComparison(prop, param) {
+	return JSON.stringify(prop) === JSON.stringify(param);
+}
+
+// function jsonSortedComparison(prop, param) {
+// 	return JSON.stringify(prop).split('').sort(sortingFunc).join('') === JSON.stringify(param).split('').sort(sortingFunc).join('');
+// }
+
+
+// function sortingFunc(x, y) {
+// 	if(x < y) return -1;
+// 	if(x > y) return 1;
+// 	if(x === y) return 0;
+// }
