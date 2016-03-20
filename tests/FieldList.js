3db308e3ef23e1437c0089aa2c4587cb45a05582
Justin Barabander <jbarabander@gmail.com>
2016-03-20 02:19:24 -0400

    added to tests

--- tests/FieldList.js
+++ tests/FieldList.js
@@ -1,1 +1,35 @@
-var assert = require('chai').assert;
+var expect = require('chai').expect;
+var FieldList = require('../src').FieldList;
+
+describe('FieldList', function() {
+	var model = {
+		strTest: 'foo',
+		numTest: 2,
+		arrTest: [1, 'bar', [], {}],
+		objTest: {}
+	}
+	var modelComparer = new FieldList();
+	modelComparer.register('testValidator', function(prop, value) {
+		return prop === value;
+	})
+
+	modelComparer.register('lengthValidator', function(prop, minLength, maxLength) {
+		if(prop.length === undefined) {
+			return true;
+		} 
+		return minLength < prop.length && prop.length < maxLength;
+	})
+
+	it('should validate correctly with custom created validators', function(done) {
+		var comparisonFields = {
+			strTest: [{validator: 'lengthValidator', params: [1, 8]}, {validator: 'testValidator', param: 'foo'}],
+			numTest: 'formlessIsNumber',
+			arrTest: [{validator: 'lengthValidator', params: [2, 5]}, {validator: 'formlessRequired'}],
+			objTest: [{validator: 'formlessRequired'}, {validator: 'formlessStrictEquals', param: {}}]
+		}
+		var validationResult = modelComparer.validateModel(model, comparisonFields);
+		// console.log(validationResult);
+		expect(validationResult.strTest.passed).to.be.true;
+		done();
+	});
+})
