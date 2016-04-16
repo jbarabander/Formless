0971e8b78e5ca08a6ce89d23fe27eee9d6c0b601
Justin Barabander <jbarabander@gmail.com>
2016-04-16 12:18:23 -0400

    Added in TODO for deep equals

--- src/builtInValidation/deepEquals.js
+++ src/builtInValidation/deepEquals.js
@@ -1,22 +1,24 @@
-module.exports = function(prop, param) {
-	if(typeof param !== 'object' || typeof prop !== 'object') {
-		return false;
-	}
-	var propKeys = 
+//TODO: finish this validator
 
-}
+// module.exports = function(prop, param) {
+// 	if(typeof param !== 'object' || typeof prop !== 'object') {
+// 		return false;
+// 	}
+// 	var propKeys = 
+
+// }
 
 function jsonComparison(prop, param) {
 	return JSON.stringify(prop) === JSON.stringify(param);
 }
 
-// function jsonSortedComparison(prop, param) {
-// 	return JSON.stringify(prop).split('').sort(sortingFunc).join('') === JSON.stringify(param).split('').sort(sortingFunc).join('');
-// }
+function jsonSortedComparison(prop, param) {
+	return JSON.stringify(prop).split('').sort(sortingFunc).join('') === JSON.stringify(param).split('').sort(sortingFunc).join('');
+}
 
 
-// function sortingFunc(x, y) {
-// 	if(x < y) return -1;
-// 	if(x > y) return 1;
-// 	if(x === y) return 0;
-// }
+function sortingFunc(x, y) {
+	if(x < y) return -1;
+	if(x > y) return 1;
+	if(x === y) return 0;
+}
